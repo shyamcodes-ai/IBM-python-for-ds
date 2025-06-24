8df130f29effcde70a73ceee7f49aa3413996576
Shyam Venkat Gonnabatthula <shyamvenkat.ai@gmail.com>
2025-06-24 22:11:45 +0530

    Add files via upload

---
+++ hello_python.py
@@ -0,0 +1,9 @@
+# hello_python.py
+# Practice from Module 1 - IBM Python for Data Science
+
+name = "Python for Data Science"
+print(f"Welcome to {name}!")
+a = 10
+b = 5
+print("Sum:", a + b)
+print("Product:", a * b)
